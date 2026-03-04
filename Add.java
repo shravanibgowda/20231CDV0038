73ad33f2a042d4a01d3ab44490e35e6f97e2ed5b
shravanibgowda <shravanibyregowda@gmail.com>
2026-03-04 11:35:58 +0530

    Create Add class to determine even or odd numbers
    Implement main method to check if a number is even or odd.

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add {
+    public static void main(String[] args) {
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } 
+        else {
+            System.out.println(n + " is odd");
+        }
+    }
+}
